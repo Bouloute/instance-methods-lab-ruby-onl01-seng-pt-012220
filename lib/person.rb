f3833db5d85c308a1934abbe4d59f53da0d57cf2
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-08 20:32:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,11 @@ class Person
   def talk
     puts "Hello World!"
   end
+<<<<<<< HEAD
 
   def walk
     puts "The Person is walking"
   end
+=======
+>>>>>>> 22fd6ff3ada8ed6d26e0528a46ecdf27e82fdea2
 end
